e4f582e765bfb71f8e1415bdff06e828d942f3ab
madvil2 <3738564d@gmail.com>
2025-07-12 20:01:25 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   optimizeDeps: {
-    include: ['react', 'react-dom', '@privy-io/react-auth'],
+    include: [
+      'react',
+      'react-dom',
+      '@headlessui/react',
+      '@privy-io/react-auth',
+      '@vercel/analytics',
+      'events',
+      'hls.js',
+      'mic-check',
+      'qrcode',
+      'react-audio-visualize',
+      'react-router-dom',
+      'web-audio-player'
+    ],
+    exclude: []
   },
   build: {
     rollupOptions: {
       onwarn(warning, warn) {
-        if (warning.code === 'INVALID_ANNOTATION') {
-          return;
-        }
+        if (warning.code === 'INVALID_ANNOTATION') return;
         warn(warning);
-      },
-    },
-  },
-});
+      }
+    }
+  }
+})
